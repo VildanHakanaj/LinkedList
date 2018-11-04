e98d8824e50423d0113889b4785ee0a288f558f1
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2018-11-04 16:07:32 -0500

    Added the add method and the printing method for linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -6,8 +6,53 @@ public class LinkedList<T> {
         makeEmpty();
     }
 
+    //region Main Methods
+
+
+    /*
+    * Add method will add the item
+    * given at the end of the list
+    *
+    * @param => T item : Item to be inserted
+    * @return => void.
+    * */
+    public void Add(T item){
+        Node<T> pointer = head;                     //Pointer to be used to nav the list
+
+        if(isEmpty()){                              //Check if the list is empty
+            head = new Node<T>(item);               //Set the new node to be the head
+        }else{
+            while(pointer.getNext() != null){       //Else check if there is the need to move the pointer
+                pointer = pointer.getNext();        //Move the pointer to the next node
+            }
+            pointer.setNext(new Node<T>(item));     //Check if the
+        }
+        count++;
+    }
+
+    public void Add(T item, int pos){
+
+    }
+    //endregion
 
     //region Helper Methods
+
+    /*
+    * Print the nodes
+    * and the items in them
+    * */
+    public void Print(){
+        Node<T> pointer = head;
+        if(isEmpty()){
+            System.out.println("The list is empty.");
+        }else{
+            while(pointer.getNext() != null){
+                System.out.printf("[ %s ]=>", pointer.getItem());
+                pointer = pointer.getNext();
+            }
+            System.out.printf("[ %s ] ==|", pointer.getItem());
+        }
+    }
 
     /*
     * Check if  the list is empty
