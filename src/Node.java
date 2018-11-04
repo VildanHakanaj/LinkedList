6637b9fe58f02a5fb9c627d1894c9510fdd7198b
Vildan Hakanaj <vildanhakanaj@trentu.ca>
2018-11-04 14:35:20 -0500

    Added the node constructor and the getters and setter

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,30 @@
 public class Node<T> {
+    private T item;
+    private Node next;
 
+    public Node(T item){
+        this.item = item;
+        this.next = null;
+    }
+
+    public Node(T item, Node next){
+        this.item = item;
+        this.next = next;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
